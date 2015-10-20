3875a4370c370fbb75d4dd6d2045afbc6367adfd
Christine Ho <christine_ho@hotmail.co.uk>
2015-10-20 18:51:33 +0100

    modified

--- app/js/dataController.js
+++ app/js/dataController.js
@@ -9,17 +9,18 @@ function DataController($http) {
 
   self.country;
   self.trends;
-
+  self.fx;
 
   function getCountry() {
     $http
       .get('http://localhost:3000/countries')
       .then(function(response) {
         console.log(response.data);
-        console.log(typeof response.data);
+        console.log(response.data.trends);
         
         self.country = response.data.country;
         self.trends = response.data.trends;
+        self.fx = response.data.fx;
       })
   }
 
